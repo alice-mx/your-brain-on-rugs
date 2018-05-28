362e12b1ab8e3ab485d069fa5eb5ad9e69e419f7
AliceAndJikai <alicem@thoughtworks.com>
2018-05-28 10:51:38 +0800

    Connected more readings to the visualisation and added a smooth transition between readings.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,60 @@
 import './style.css'
 
-function component() {
-  var element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = "Hi";
-
-  return element;
-}
-
-document.body.appendChild(component());
-
 var prevHeight = 200;
 var height = 200;
-
+var width = 50;
+var prevWidth = 50;
+var saturation = 100;
+var prevSaturation = 100;
+var prevSpeed =  100;
+var speed = 100;
 setInterval(function() {
   fetch('http://localhost:3000').then(response => {
     response.json().then(json => {
       let data = json;
       console.log(data);
       prevHeight = height;
-      if(typeof(data.eegPower) != 'undefined') {
-        var newHeight = data.eegPower.highGamma/20;
-        var update = setInterval(function() {
-          var difference = height - newHeight;
-          if(difference > 10) {
-            height -=1;
-          } else if (difference < -10) {
-            height +=1;
-          } else {
-            clearInterval(update);
+      prevWidth = width;
+
+      if(typeof(data.eSense)!='undefined') {
+        var saturationStep = ((data.eSense.attention)-prevSaturation)/100;
+        var speedStep = ((data.eSense.meditation)-prevSpeed)/100;
+        var c = 0;
+
+        var smoothUpdate = setInterval(function() {
+          saturation = prevSaturation + (saturationStep*i);
+          speed = prevSpeed + (speedStep*i);
+
+          document.getElementById("saturation").innerHTML = saturation;
+          document.getElementById("speed").innerHTML = speed;
+          c++;
+          if(c==50) {
+            clearInterval(smoothUpdate);
           }
-        },1);
+        },20);
       }
+
+      if(typeof(data.eegPower) != 'undefined') {
+        var step = ((data.eegPower.highGamma/20)-prevHeight)/50;
+
+        var widthStep = ((data.eegPower.delta/2000)-prevWidth)/50;
+
+        var i = 0;
+
+        var smoothUpdate = setInterval(function() {
+          height = prevHeight + (step*i);
+          width = prevWidth + (widthStep*i);
+          document.getElementById("height").innerHTML = height;
+          document.getElementById("width").innerHTML = width;
+          i++;
+          if(i==50) {
+            clearInterval(smoothUpdate);
+
+          }
+        },10);
+
+      }
+
     });
   });
 }, 1000);
@@ -59,8 +81,6 @@ var body = document.querySelector('.background');
 body.appendChild(canvas);
 
 var context = canvas.getContext("2d");
-var widthInput = document.getElementById('width');
-var speedInput = document.getElementById('speed');
 var sizeInput = document.getElementById('circle');
 var forumla = document.getElementById('formula');
 var clearCanvas = document.getElementById('clear');
@@ -70,14 +90,11 @@ var i = 0;
 
 
 setInterval(function() {
-  formula.innerHTML = `y = sin ( x / ${widthInput.value} + t / ${(10000-speedInput.value)} ) * ${height} + 400`;
-  if(clear.checked) {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-  }
+  formula.innerHTML = `y = sin ( x / ${width} + t / ${(10000-speed)} ) * ${height} + 400`;
 
-  for(var x = 100 ; x < 900; x++) {
-    var y = getYCoordsForCurve(x, widthInput.value, speedInput.value, height);
-    context.strokeStyle = `hsla(${x/3+i/1000%360}, 100%, ${50+x/20}%, 0.8)`;
+  for(var x = -100 ; x < 1100; x++) {
+    var y = getYCoordsForCurve(x, width, speed, height);
+    context.strokeStyle = `hsla(${x/3+i/1000%360}, ${saturation}%, ${100-saturation/2}%, 0.8)`;
     context.beginPath();
     context.lineWidth=1;
     context.arc(x,y, sizeInput.value, 100, 2 * Math.PI, true);
